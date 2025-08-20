ae729ad03210a60929087b29f2c0e1226f312eb4
BlockLune <39331194+BlockLune@users.noreply.github.com>
2025-08-20 19:55:15 +0800

    chore: remove queries

--- queries/injections.scm
+++
@@ -1,7 +0,0 @@
-((raw_text) @injection.content
-  (#set! injection.language "javascript")
-  (#set! injection.include-children))
-
-((expression) @injection.content
-  (#set! injection.language "javascript")
-  (#set! injection.include-children))
